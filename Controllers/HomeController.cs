d4a5a5276c82bea721c23b9ae018a9ba159efa87
sudhi <sudhi@SUDHIR_KANAKOTI>
2022-09-09 14:43:54 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SSP_Events.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "𝗦𝗧𝗢𝗥𝗜𝗘𝗦 𝗕𝗬 𝗦𝗥𝗜𝗡𝗜𝗩𝗔𝗦 𝗣𝗨𝗧𝗧𝗔";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "SSP contact Details";
+
+            return View();
+        }
+
+        public ActionResult InstaReels()
+        {
+            ViewBag.Message = "Insta Reels";
+
+            return View();
+        }
+    }
+}
